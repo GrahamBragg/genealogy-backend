08aeed284ae68421fdd26a2d2ed6a626fe5957cb
Simon Bragg <simon.bragg+github@gmail.com>
2021-09-10 15:42:05 +1000

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: {
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  } });
 
   // Config
   const configService: ConfigService = app.get(ConfigService);
